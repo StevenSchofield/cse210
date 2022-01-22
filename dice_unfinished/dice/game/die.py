d8f7a2c865767d7a9fac26b6516f18a15699fea3
StevenSchofield <sch21093@byui.edu>
2022-01-21 17:09:14 -0800

    Added and finished dice program
    Added all of the unfinished dice program files, and then completed them. There now is a fully functioning 'game' in these files, despite the fact that I am not changing the name of the folder.

---
+++ dice_unfinished/dice/game/die.py
@@ -0,0 +1,39 @@
+from array import array
+import random
+
+class Die:
+    """A small cube with a different number of spots on each of its six sides.
+
+    The responsibility of Die is to keep track of the side facing up and calculate the points for 
+    it.
+   
+    Attributes:
+        value (int): The number of spots on the side facing up.
+        points (int): The number of points the die is worth.
+    """
+
+    def __init__(self):
+        """Constructs a new instance of Die with a value and points attribute.
+
+        Args:
+            self (Die): An instance of Die.
+        """
+        self.value = 0
+        self.points = 0
+
+    def roll(self):
+        """Generates a new random value and calculates the points.
+        
+        Args:
+            self (Die): An instance of Die.
+        """
+        self.value = random.randint(1, 6)
+
+        match self.value:
+            case 1:
+                self.points = 100
+            case 5:
+                self.points = 50
+            case _:
+                self.points = 0
+            
