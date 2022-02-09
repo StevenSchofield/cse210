08cb853e54bccddd8173caeb6a08ef61e9fe1d20
StevenSchofield <sch21093@byui.edu>
2022-02-09 09:14:20 -0800

    Encapsulated air_tank and balloon classes

--- air/game/balloon.py
+++ air/game/balloon.py
@@ -1,41 +1,41 @@
 class Balloon:
     def __init__(self, color):
-        self.popped = False
-        self.sealed = False
-        self.color = color
-        self.volume_ml = 0
-        self.max_volume_ml = 1000
-        self.update_time_ms = 0
+        self._popped = False
+        self._sealed = False
+        self._color = color
+        self._volume_ml = 0
+        self._max_volume_ml = 1000
+        self._update_time_ms = 0
     
     def pop(self):
-        self.popped = True
-        self.volume_ml = 0
+        self._popped = True
+        self._volume_ml = 0
 
     def decrease_air(self, delta_ms):
-        self.volume_ml -= (self.volume_ml/2) * delta_ms
+        self._volume_ml -= (self._volume_ml/2) * delta_ms
     
     def fill(self, volume_ml):
-        if not self.popped:
-            self.volume_ml += volume_ml
+        if not self._popped:
+            self._volume_ml += volume_ml
 
     def seal(self):
-        self.sealed = True
+        self._sealed = True
 
     def update(self, time_ms):
-        delta_ms = time_ms - self.update_time_ms
+        delta_ms = time_ms - self._update_time_ms
 
-        if self.popped:
-            self.volume_ml = 0
-        elif not self.sealed and self.volume_ml > 0:
+        if self._popped:
+            self._volume_ml = 0
+        elif not self._sealed and self._volume_ml > 0:
             self.decrease_air(delta_ms)
-        elif self.volume_ml > self.max_volume_ml:
+        elif self._volume_ml > self._max_volume_ml:
             self.pop()
 
-        self.update_time_ms = time_ms
+        self._update_time_ms = time_ms
 
     def __str__(self):
-        if self.popped:
-            return "<A mess of " + self.color + " elastic shreds>"
+        if self._popped:
+            return "<A mess of " + self._color + " elastic shreds>"
         else:
-            return self.color + ": " + str(self.volume_ml)
+            return self._color + ": " + str(self._volume_ml)
 
